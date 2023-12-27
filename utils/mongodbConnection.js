f3ddaca41c6e2abc4526bdc26b52bde2524178ca
ali99944 <ali99944>
2023-12-27 14:02:20 +0200

    first commit

--- utils/mongodbConnection.js
+++ utils/mongodbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb://127.0.0.1:27017/technicians'; // Replace with your MongoDB connection URI
+const uri = process.env.MONGODB_CONNECTION_STRING; // Replace with your MongoDB connection URI
 
 
 mongoose.connect(uri).then(() => console.log('connected mongoose'))
